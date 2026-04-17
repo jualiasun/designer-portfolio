d867420670abc1e405b8794dee67be5c3b0d4547
Yurii Hupalo <uragupalo@gmail.com>
2026-04-17 17:11:03 +0200

    Add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: 'Boldonse',
+      cssVariable: '--font-boldonse',
+    },
+    {
+      provider: fontProviders.local(),
+      name: 'AktivGrotesk',
+      cssVariable: '--font-aktiv-grotesk',
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/AktivGroteskCorp-Regular.woff2'],
+            weight: 'normal',
+            style: 'normal',
+          },
+          {
+            src: ['./src/assets/fonts/AktivGroteskCorp-Bold.woff2'],
+            weight: 'bold',
+            style: 'normal',
+          },
+        ],
+      },
+    },
+  ],
+})
