03080abdef29cdc4d0114e96194f9dcbe679e2b6
Yurii Hupalo <uragupalo@gmail.com>
2026-04-28 19:58:00 +0200

    Add Behance banner

---
+++ src/data/links.ts
@@ -0,0 +1,6 @@
+export const externalLinks = {
+  linkedin: 'https://www.linkedin.com/in/yuliiaolomska',
+  instagram: 'https://www.instagram.com/jualiasun',
+  behance: 'https://www.behance.net/juliaolomska',
+  email: 'mailto:juliaolomska@gmail.com',
+} as const
